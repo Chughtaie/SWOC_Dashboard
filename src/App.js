5153217d271dba726162ac9228d3a28219d4f092
abdullah-akram <mabdullahakram2000@gmail.com>
2022-10-26 23:38:26 +0500

    table created

--- src/App.js
+++ src/App.js
@@ -1,112 +1,186 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useState, useRef } from "react";
-import { MapContainer, TileLayer, Marker, Popup, } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import Routing from "./RoutingMachine";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Table from "react-bootstrap/Table";
 
 const center = [33.6765861775585, 73.03936673659675];
 const mark = new L.Icon({
-  iconUrl: require("./bin.jpg"),
-  iconRetinaUrl: require("./bin.jpg"),
-  iconAnchor: null,
-  popupAnchor: null,
-  shadowUrl: null,
-  shadowSize: null,
-  shadowAnchor: null,
-  iconSize: new L.Point(60, 75),
-  className: "leaflet-div-icon",
+	iconUrl: require("./bin.png"),
+	iconRetinaUrl: require("./bin.png"),
+	iconAnchor: null,
+	popupAnchor: null,
+
+	shadowUrl: null,
+	shadowSize: null,
+	shadowAnchor: null,
+	iconSize: new L.Point(40, 55),
+	className: "leaflet-div-iconn",
 });
 
 const binLocations = [
-  [33.6765861775585, 73.03936673659675],
-  [33.67777003432862, 73.01396972440627],
-  [33.65061547528435, 73.02524088532327],
-  [33.64783561289653, 72.95331076988744],
-  [33.66839027456748, 73.07530059035655]
-//   [33.643616586980076, 72.28377792894402],
+	[33.6765861775585, 73.03936673659675],
+	[33.67777003432862, 73.01396972440627],
+	[33.65061547528435, 73.02524088532327],
+	[33.64783561289653, 72.95331076988744],
+	[33.66839027456748, 73.07530059035655],
+	//   [33.643616586980076, 72.28377792894402],
 ];
 const truckLocations = [33.66754776310643, 73.0032501092188];
-const routes= [truckLocations].concat(binLocations);
+const routes = [truckLocations].concat(binLocations);
 const truck = new L.Icon({
-  iconUrl: require("./logo.png"),
-  iconRetinaUrl: require("./logo.png"),
-  iconAnchor: null,
-  popupAnchor: null,
-  shadowUrl: null,
-  shadowSize: null,
-  shadowAnchor: null,
-  iconSize: new L.Point(95, 75),
-  className: "leaflet-div-icon",
+	iconUrl: require("./logo.png"),
+	iconRetinaUrl: require("./logo.png"),
+	iconAnchor: null,
+	popupAnchor: null,
+	shadowUrl: null,
+	shadowSize: null,
+	shadowAnchor: null,
+	iconSize: new L.Point(105, 65),
+	className: "leaflet-div-iconn",
 });
+
+function handleClick() {
+	alert("Hello!");
+}
+
 function App() {
-  return (
-    <div className="App">
-      <div className="navbar">
-        <img src={logo} alt="logo" />
-        <text> Admin Dashboard</text>
-      </div>
-      <div className="body">
-        <div className="list">
-          Following vehicles have the shortest route
-          <ol>
-            <li className="list_element">
-              <button className="button-36"> Private Contractor 1</button>
-            </li>
-            <li className="list_element">
-              <button className="button-36"> Official Contractor 1</button>
-            </li>
-            <li className="list_element">
-              {" "}
-              <button className="button-36">Private Contractor 2</button>
-            </li>
-          </ol>
-        </div>
-        <MapContainer
-          center={center}
-          zoom={16}
-          style={{ width: "200vw", height: "100vh" }}
-        >
+	const [volume, setVolume] = useState(1);
+	const [muted, setMuted] = useState(false);
 
-          <TileLayer
-            url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=67tzsplQZ74XQ5pgVnPj"
-            attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-          />
+	const finalVolume = muted ? 0 : volume ** 1;
+	return (
+		<div className="App">
+			<Navbar className="navv">
+				<Container>
+					SWOC Dashboard
+					<Nav>
+						<div className="navb">
+							<Nav.Link href="#home">Trucks</Nav.Link>
+							<Nav.Link href="#features">Bins</Nav.Link>
+							<Nav.Link href="#pricing">Home</Nav.Link>
+						</div>
+					</Nav>
+				</Container>
+			</Navbar>
 
+			<br />
 
+			<div className="body">
+				<MapContainer
+					center={center}
+					zoom={16}
+					style={{ width: "200vw", height: "100vh" }}
+				>
+					<TileLayer
+						url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=67tzsplQZ74XQ5pgVnPj"
+						attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+					/>
 
-   
-		 	  {routes.map((row, index)=> { if(index < routes.length-1)
-			  return(
-			<Routing source={{lat:routes[index][0], lng:routes[index][1]}} destination={{lat:routes[index+1][0], lng:routes[index+1][1]}}/>
-  
-      )})}     
-		  {binLocations.map((item) => {
-            return (
-              <Marker
-                icon={mark}
-                position={[item[0], item[1]]}
-                style="ba"
-              ></Marker>
-            );
-          })}
-          {
-            <Marker
-              icon={truck}
-              position={truckLocations}
-              style="ba"
-            ></Marker>
-          }
-        </MapContainer>
+					{routes.map((row, index) => {
+						if (index < routes.length - 1)
+							return (
+								<Routing
+									source={{
+										lat: routes[index][0],
+										lng: routes[index][1],
+									}}
+									destination={{
+										lat: routes[index + 1][0],
+										lng: routes[index + 1][1],
+									}}
+								/>
+							);
+					})}
+					{binLocations.map((item) => {
+						return (
+							<Marker
+								icon={mark}
+								position={[item[0], item[1]]}
+								style="ba"
+								className="leaflet-div-iconn"
+							></Marker>
+						);
+					})}
+					{
+						<Marker
+							icon={truck}
+							position={truckLocations}
+							style="ba"
+						></Marker>
+					}
+				</MapContainer>
 
-        <div className="buttons">
-          <button className="button-36">Run Optimization</button>
-          <button className="button-36">Set Threshold</button>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="buttons">
+					<button className="button-36" onClick={handleClick}>
+						Run Optimization
+					</button>
+					<br />
+					<p
+						style={{
+							color: "#ffff",
+							fontStyle: "Bold",
+							fontFamily: "Arial",
+						}}
+					>
+						Set Threshold
+					</p>
+
+					<input
+						type="range"
+						min={0}
+						max={100}
+						step={2}
+						value={volume}
+						onChange={(event) => {
+							setVolume(event.target.valueAsNumber);
+						}}
+					/>
+					{finalVolume.toFixed(1)}
+				</div>
+			</div>
+			<div className="table">
+				<Table>
+					<thead>
+						<tr>
+							<th>Truck ID #</th>
+							<th>Optimized Path</th>
+							<th>Distance</th>
+							<th>Details</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>12394</td>
+							<td>20</td>
+							<td>Details</td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>12394</td>
+							<td>20</td>
+							<td>Details</td>
+						</tr>
+
+						<tr>
+							<td>3</td>
+							<td>12394</td>
+							<td>20</td>
+							<td>Details</td>
+						</tr>
+					</tbody>
+				</Table>
+			</div>
+			<div> .</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBufeYzH30v2ksHUQnjUY1Cmo4nkRE21h0",
+	authDomain: "first-cloud-messaging-c65bf.firebaseapp.com",
+	projectId: "first-cloud-messaging-c65bf",
+	storageBucket: "first-cloud-messaging-c65bf.appspot.com",
+	messagingSenderId: "821607945473",
+	appId: "1:821607945473:web:d4b8242e8bfdbe4c7df700",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
